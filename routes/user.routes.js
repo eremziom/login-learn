c8fc4a2edf2615eae666e0a3b9b18af229dd79ec
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-24 10:49:19 +0100

    add login autho to user pages

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  if(req.user){
+    res.render('logged');
+  }
+  else {
+    res.redirect('/user/no-permission')
+  }
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
+
+router.get('/profile', (req, res) => {
+  if(req.user){
+    res.render('profile');
+  }
+  else {
+    res.redirect('/user/no-permission')
+  }
+})
+
+router.get('/profile/settings', (req, res) => {
+  if(req.user){
+    res.render('profile-settings');
+  }
+  else {
+    res.redirect('/user/no-permission')
+  }
+})
 
 
 module.exports = router;
